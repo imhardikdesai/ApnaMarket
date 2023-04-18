00a7163d51a53705dadcdb83b4a0cb07af6ecfbe
Hardik Desai <124128351+hardikd-zignuts@users.noreply.github.com>
2023-04-18 14:31:23 +0530

    feat: :zap: Copy Paste Code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes/routes";
+import ScrollToTop from "./components/scroll-to-top/ScrollToTop";
+import ThemeProvider from "./theme";
 
 function App() {
   return (
-    'Hello'
+    <BrowserRouter>
+      <ThemeProvider>
+        <ScrollToTop />
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
